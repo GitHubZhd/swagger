4f0dd30d16caf20417743bee87139d3358716898
zhanghaidong <zhanghaidong@daihoubang.com>
2018-03-07 19:13:04 +0800

    submit

---
+++ src/main/java/com/pengshu/core/SwaggeerApplication.java
@@ -0,0 +1,23 @@
+package com.pengshu.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@ComponentScan("com.*")
+@SpringBootApplication
+public class SwaggeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pengshu/core/SwaggeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pengshu.core;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SwaggeerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
